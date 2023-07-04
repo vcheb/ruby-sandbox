69ff5fa860fa4d29950a2445582f72ad48d17c11
vcheb <137329406+vcheb@users.noreply.github.com>
2023-07-04 17:59:41 +0000

    lesson50

---
+++ debugger.rb
@@ -0,0 +1,9 @@
+require "pry-byebug"
+
+f = "Your lucky number is "
+
+l = rand(100)
+
+byebug
+
+pp f + l
---
+++ howdy.rb
@@ -0,0 +1,11 @@
+my_string = "Hello, world!"
+pp my_string
+
+require "./goodbye.rb"
+require "active_support/all"
+
+pp "What's your name?"
+
+their_name = gets
+
+puts "Hello, " + their_name + "!"
